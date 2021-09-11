0f52027ecf46a830f807ec6bf3207e6f69278fbf
arvind-balaji <arvindb02@gmail.com>
2021-09-11 16:08:40 -0500

    Add ImageTile

---
+++ components.js/ImageTile.js
@@ -0,0 +1,26 @@
+import { Flex, Box, Heading } from '@chakra-ui/react';
+
+const ImageTile = ({ text, image, size = 'xl', ...rest }) => {
+  return (
+    <Flex
+      minH="45vh"
+      backgroundColor="#434443"
+      borderRadius={25}
+      px={10}
+      py={5}
+      color="white"
+      {...rest}
+      {...(image && {
+        backgroundSize: 'contain',
+        backgroundPosition: 'center',
+        backgroundRepeat: 'no-repeat',
+        backgroundImage: `url("/${image}")`,
+      })}>
+      <Box>
+        <Heading size={size}>{text}</Heading>
+      </Box>
+    </Flex>
+  );
+};
+
+export default ImageTile;
