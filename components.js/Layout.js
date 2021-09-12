fe469b722fc5a70e83e1abb84b32bb81077192e8
Kirthivel <kirthivel@gmail.com>
2021-09-12 10:54:13 -0500

    layout changes

--- components.js/Layout.js
+++ components.js/Layout.js
@@ -1,12 +1,16 @@
-import { Box, Flex } from "@chakra-ui/layout";
+import { Box, Flex, Image } from "@chakra-ui/react";
 // import { FC } from "react";
 // import { Header } from "./Header";
 import Head from "next/head";
+
 export const Layout = ({ children }) => (
     <Box backgroundColor="gray.200" minHeight="100vh">
         {/* <Header /> */}
         <Flex height="70" bgColor="white" alignItems="center" px={30}>
-            <Box h="30" w="40" bgColor="green.100"></Box>
+            {/* <Box h="30" w="40" bgColor="green.100" bgImage = "logo" ></Box> */}
+            <Image 
+            src="/headerlogo.png"
+            h="42" w="46" marginLeft="50" /> 
         </Flex>
         <Head>
             <meta name="viewport" content="width=device-width, initial-scale=1" />
